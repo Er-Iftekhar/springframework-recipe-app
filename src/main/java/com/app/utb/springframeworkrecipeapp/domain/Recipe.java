61617201f4fc74e845464f6fc5e0891ee6bb42b4
SyedIftekharuddin <syedy636@gmail.com>
2020-11-26 19:27:20 +0100

    Ingredient entity created and OTM relation added between Recipe and Ingredient.

--- src/main/java/com/app/utb/springframeworkrecipeapp/domain/Recipe.java
+++ src/main/java/com/app/utb/springframeworkrecipeapp/domain/Recipe.java
@@ -1,6 +1,8 @@
 package com.app.utb.springframeworkrecipeapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -21,6 +23,13 @@ public class Recipe {
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToMany(
+            targetEntity = Ingredient.class,
+            mappedBy = "recipe",
+            cascade = CascadeType.ALL
+    )
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -100,5 +109,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
