c1d25dc8139bdd3fd21d683fe4f46281e85eb0d4
SyedIftekharuddin <syedy636@gmail.com>
2020-11-26 19:05:39 +0100

    Added Recipe and Notes model classes, and OTO relation between them.

--- src/main/java/com/app/utb/springframeworkrecipeapp/SpringframeworkRecipeAppApplication.java
+++ src/main/java/com/app/utb/springframeworkrecipeapp/SpringframeworkRecipeAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringframeworkRecipeAppApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringframeworkRecipeAppApplication.class, args);
+        System.out.println("Hello ");
     }
 
 }
