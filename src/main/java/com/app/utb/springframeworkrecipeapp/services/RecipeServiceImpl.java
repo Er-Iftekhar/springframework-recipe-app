57a804ff2cb8c4402271493fd816cd670980f1ee
SyedIftekharuddin <syedy636@gmail.com>
2020-11-29 10:23:27 +0100

    Added project lombok and implemented in Recipe class.

--- src/main/java/com/app/utb/springframeworkrecipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/app/utb/springframeworkrecipeapp/services/RecipeServiceImpl.java
@@ -2,12 +2,14 @@ package com.app.utb.springframeworkrecipeapp.services;
 
 import com.app.utb.springframeworkrecipeapp.domain.Recipe;
 import com.app.utb.springframeworkrecipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
+@Slf4j
 public class RecipeServiceImpl implements RecipeService{
     private final RecipeRepository recipeRepository;
 
@@ -17,6 +19,7 @@ public class RecipeServiceImpl implements RecipeService{
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I am in the recipe service implementation");
         Set<Recipe> allRecipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(allRecipes::add);
         return allRecipes;
