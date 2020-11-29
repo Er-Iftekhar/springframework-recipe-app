1d1167fc23a26d3dffe7468dc59cd75e9f050530
SyedIftekharuddin <syedy636@gmail.com>
2020-11-29 12:14:17 +0100

    Refractor the classed for stackoverflow error and Lazyinitialization excetions.

--- src/main/java/com/app/utb/springframeworkrecipeapp/controller/IndexController.java
+++ src/main/java/com/app/utb/springframeworkrecipeapp/controller/IndexController.java
@@ -1,11 +1,13 @@
 package com.app.utb.springframeworkrecipeapp.controller;
 
 import com.app.utb.springframeworkrecipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -15,9 +17,9 @@ public class IndexController {
         this.recipeService = recipeService;
     }
 
-
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model){
+        log.debug("Index Controller with recipes");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
--- src/main/java/com/app/utb/springframeworkrecipeapp/domain/Ingredient.java
+++ src/main/java/com/app/utb/springframeworkrecipeapp/domain/Ingredient.java
@@ -1,24 +1,27 @@
 package com.app.utb.springframeworkrecipeapp.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
+@EqualsAndHashCode(exclude = "recipe")
 @Entity
 public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
+
     private BigDecimal amount;
-
-
 
     @ManyToOne
     private Recipe recipe;
 
-    @OneToOne(
-            targetEntity = UnitOfMeasure.class
-    )
+    @OneToOne(targetEntity = UnitOfMeasure.class)
     private UnitOfMeasure uom;
 
     public Ingredient() {
@@ -30,45 +33,4 @@ public class Ingredient {
         this.uom = uom;
     }
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-
-        this.recipe = recipe;
-    }
-
-    public UnitOfMeasure getUom() {
-        return uom;
-    }
-
-    public void setUom(UnitOfMeasure uom) {
-        this.uom = uom;
-    }
 }
